b7ec5caa997d52af2dfafbbb04f03833304ee825
ahmedalhakeem <ahmkam2011@gmail.com>
2021-11-10 23:37:49 +0300

    adding modal

--- eticket/static/eticket/ticket_reply.js
+++ eticket/static/eticket/ticket_reply.js
@@ -1,38 +1,60 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    // const allocate_btn = document.querySelector('#allocate-btn')
-    // console.log(allocate_btn.parentElement.children);
+    const select = document.querySelector('#choose-day')
+    console.log(select);
     const wrap = document.querySelector('#wrap')
+    wrap.style.display = 'none'
 
-    const allocation = document.querySelector('.allocate-btn');
-    const table = document.querySelector('#tableid')
-    const rows = document.getElementsByTagName('tr')
-    for(i=1; i<=rows.length; i++){
-        let row = table.rows[i]
-        // console.log();
-        row.onclick = function(){
-            let cell = this.getElementsByTagName('td')[5];
-            console.log(cell);
-        }
-    }
-    console.log(table);
-})
-    // allocation.addEventListener('click', (e)=>{
-    //     e.preventDefault()
-    //     const parent = e.target.parentElement.parentElement
-    //     const num_days = parent.children[5].innerHTML
-    //     console.log(typeof(num_days));
-    //     console.log(parent.children[5]);
-    //     const days =  document.querySelector('#tour-duration').innerHTML
-    //     const integer = Number(days);
-    //     const select = document.createElement('select')
-    //     for(let i=1 ; i<= integer; i++){
-    //         const option = document.createElement('option')
-    //         option.innerHTML = `day ${i}`
-    //         select.append(option)
-    //         wrap.append(select);
-    //     }
-    //     console.log(select); 
-           
-    // })
-    
+  
 
+        // $('.allocate-btn').on('click', function(e){
+        //     select.innerHTML= ''
+        //     if(wrap.style.display === 'none'){
+        //         wrap.style.display = 'block'
+        //     }
+        //     wrap.innerHTML=''
+        //     const currentRow = $(this).closest('tr')
+        //     const col1 = currentRow.find('td:eq(0)').text();
+        //     const col2 = currentRow.find('td:eq(1)').text();
+        //     const col5 = currentRow.find('td:eq(5)').text();
+        //     const num = Number(col5)
+            
+        //     const init_option = document.createElement('option')
+        //     init_option.value = 0
+        //     init_option.innerHTML= "choose day"
+        //     init_option.selected = true
+        //     select.append(init_option)
+        //     for(i=1; i<=num; i++){
+        //         const option = document.createElement('option')
+        //         option.innerHTML= `day ${i}`
+        //         console.log(option);
+        //         select.append(option)
+
+        //     }
+        //     wrap.append(select)
+
+        // })
+    $('#choose-day').on('change', function(){
+        alert('changed')
+    })
+          
+    $('#get-data').on('click', function(){
+        const arr = []
+        $('#tableid tbody tr').each(function(){
+            const currentRow = $(this)
+            const col1 = currentRow.find('td:eq(0)').text();
+            const col2 = currentRow.find('td:eq(1)').text();
+            const col5 = currentRow.find('td:eq(5)').text();
+
+            const obj = {}
+            obj.col1 = col1
+            obj.col2 = col2
+            obj.col5 = col5
+            if(currentRow.find('.chk').is(":checked")){
+                arr.push(obj)
+                
+            }
+        })
+        console.log(arr);
+    });
+
+});
